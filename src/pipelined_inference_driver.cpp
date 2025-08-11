00494d445c6e35efd670276b60ee45382eea6aeb
NamcheolLee <nclee@redwood.snu.ac.kr>
2025-08-11 10:46:01 +0900

    modified comments

--- src/pipelined_inference_driver.cpp
+++ src/pipelined_inference_driver.cpp
@@ -169,8 +169,8 @@ void stage2_thread_function(tflite::Interpreter* interpreter) {
         interpreter->Invoke();
         // ====================================
 
-        /* Postprocessing */
-        /* Access the 0th output tensor */
+        /* Extract the 0th output tensor, copy it into the intermediate tensor, 
+         * and push it into stage2_to_stage3_queue */
         // ======= Write your code here =======
         TfLiteTensor* output_tensor = interpreter->output_tensor(0);
         size_t size = 1;
