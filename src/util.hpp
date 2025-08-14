cd6b1f5548a705aa2de36d92ed3381430c33d9ef
NamcheolLee <nclee@redwood.snu.ac.kr>
2025-08-14 09:55:55 +0900

    revised comments

--- src/util.hpp
+++ src/util.hpp
@@ -93,7 +93,7 @@ private:
     std::queue<T> queue; // C++ standard library queue for storing items
     std::mutex mutex; // Mutex for synchronizing access to the queue
     std::condition_variable cond_var; // Used to block 'pop' until an item is available
-    std::atomic<bool> shutdown{false}; // Flag to indicate that no more items will be pushed
+    bool shutdown{false}; // Flag to indicate that no more items will be pushed
 };
 
 namespace util
