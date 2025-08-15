ca39d21cdaf4e82c38872f667a7bceb911430df8
Taehyun Kim <158986409+zzilit11@users.noreply.github.com>
2025-08-15 17:06:58 +0900

    Update model_slicer.py

--- model_slicer.py
+++ model_slicer.py
@@ -24,14 +24,14 @@ import absl.logging
 absl.logging.set_verbosity(absl.logging.ERROR)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-# Partition a Keras model object into multiple slices
+# Slice a model from the start layer to the end layer
 def slice_dnn(model, start, end, input_tensors): 
     """
     Parameters:
-        model (tf.keras.Model): Original TensorFlow model to be partitioned
+        model (tf.keras.Model): A Keras model object to be sliced
         start (int): Index of the first layer of the slice
         end (int): Index of the last layer of the slice
-        input_tensors (dict): Inputs to the slice {generating layer name: tensor}
+        input_tensors (dict): Inputs to the slice {input layer name: input tensor}
 
     Key data structures:
         input_layers (dict): Newly created layers for the slice: one layer per input tensor
@@ -41,37 +41,31 @@ def slice_dnn(model, start, end, input_tensors):
         tensors_from_current_layer (list): Tensor(s) produced by the current layer
     """
     
-    # 6-(1) Initialize data structures
-    input_layers = {}          # Newly created layers for the slice: one layer per input tensor
-    intra_slice_skips = {}     # Tensor(s) used for processing intra-slice skip connections
-    inter_slice_skips = {}     # Tensor(s) used for processing inter-slice skip connections
+    input_layers = {}          
+    intra_slice_skips = {}     
+    inter_slice_skips = {}     
 
-    # 6-(2) Create input layers 
+    # 6-(1) Create input layers 
     for name, tensor in input_tensors.items():
         input_layers[name] = tf.keras.layers.Input(shape=tensor.shape[1:], name=name)
         intra_slice_skips[name] = input_layers[name]
 
-    # 6-(3) Initialize the input tensors for the first layer in the slice
-    # Multiple inputs to the first layer in the slice
+    # 6-(2) Initialize Inputs for the start layer
+    # Multiple inputs for the start layer
     tensors_to_current_layer = [] 
     if isinstance(model.layers[start].input, list):
         for tensor in model.layers[start].input:
             key = tensor.name.split('/')[0]
-            print(tensor.name)
             val = input_layers[key]
             tensors_to_current_layer.append(val)                    
-            inter_slice_skips[key] = val 
-    # Single input to the first layer in the slice
+            inter_slice_skips[key] = val
+    # Single input for the start layer
     else:
-        if len(input_layers) == 1:
-            tensors_to_current_layer = list(input_layers.values())[0]
-        else:
-            key = model.layers[start].input.name.split('/')[0]
-            print(model.layers[start].input.name)
-            val = input_layers[key]
-            tensors_to_current_layer = val
+        key = model.layers[start].input.name.split('/')[0]
+        val = input_layers[key]
+        tensors_to_current_layer = val
 
-    # 6-(4) Iterate over layers within the specified layer index range
+    # 6-(3) Perform each layer's operation and store outputs for skip connections
     for i in range(start, end+1):
         layer = model.layers[i]
         
@@ -84,7 +78,7 @@ def slice_dnn(model, start, end, input_tensors):
                 if origin_idx != i-1:
                     tensors_from_current_layer = \
                         layer([tensors_to_current_layer, intra_slice_skips[origin_layer.name]])
-        # Single inputs for current layer
+        # Single input for current layer
         else:
             origin_layer = model.get_layer(inbound_layers.name)
             if origin_layer.name in intra_slice_skips:
@@ -92,50 +86,50 @@ def slice_dnn(model, start, end, input_tensors):
             else:
                 tensors_from_current_layer = layer(tensors_to_current_layer)
 
-        # Multiple outputs from the current layer
+        # Multiple outputs from current layer
         if len(layer._outbound_nodes)>1:
-            destination_idx = model.layers.index(layer._outbound_nodes[1].outbound_layer)
-            if destination_idx < end+1:
+            skip_target_idx = model.layers.index(layer._outbound_nodes[1].outbound_layer)
+            if skip_target_idx < end+1:
                 intra_slice_skips[layer.name] = tensors_from_current_layer
             else:
                 inter_slice_skips[layer.name] = tensors_from_current_layer
-        # Single output from the current layer
+        # Single output from current layer
         else:
             if i != len(model.layers)-1:
-                destination_idx = model.layers.index(layer._outbound_nodes[0].outbound_layer)
-                if destination_idx != i+1:
-                    if destination_idx < end+1:
+                skip_target_idx = model.layers.index(layer._outbound_nodes[0].outbound_layer)
+                if skip_target_idx != i+1:
+                    if skip_target_idx < end+1:
                         intra_slice_skips[layer.name] = tensors_from_current_layer
                     else:
                         inter_slice_skips[layer.name] = tensors_from_current_layer
 
         tensors_to_current_layer = tensors_from_current_layer
 
-    # 6-(5) Construct the output tensors of the sliced model
+    # 6-(4) Construct output tensors
     if inter_slice_skips:
         tensors_from_current_layer = [tensors_from_current_layer] + list(inter_slice_skips.values())
 
-    # 6-(6) Create and return the sliced model
-    sliced_model = \
-        tf.keras.models.Model(inputs=list(input_layers.values()), outputs=tensors_from_current_layer)
-    return sliced_model
+    # 6-(5) Create and return the slice
+    slice = tf.keras.models.Model(inputs=list(input_layers.values()), 
+                                  outputs=tensors_from_current_layer)
+    return slice
 
 # Prepare inputs for a slice
-def prepare_slice_inputs(sliced_model):
+def prepare_slice_inputs(slice):
     next_inputs = {}
-    for output, output_tensor in zip(sliced_model.outputs, sliced_model.outputs):
+    for output, output_tensor in zip(slice.outputs, slice.outputs):
         layer_name = output._keras_history[0].name
         next_inputs[layer_name] = output_tensor
     return next_inputs
 
-# Save the sliced model as a LiteRT model
-def save_sliced_model(output_dir, sliced_model, sliced_model_num):
-    # Convert the sliced model to a LiteRT model
-    converter = tf.lite.TFLiteConverter.from_keras_model(sliced_model)
+# Save the given slice as a LiteRT model
+def convert_save_slice(output_dir, slice, slice_num):
+    # Convert the slice to a LiteRT model
+    converter = tf.lite.TFLiteConverter.from_keras_model(slice)
     litert_model = converter.convert()
     # Save the LiteRT model to the specified output directory
     os.makedirs(output_dir, exist_ok=True)
-    filename = f"submodel_{sliced_model_num}.tflite"
+    filename = f"submodel_{slice_num}.tflite"
     litert_path = os.path.join(output_dir, filename)
     with open(litert_path, 'wb') as f:
         f.write(litert_model)
@@ -150,11 +144,11 @@ def parse_arguments():
     parser.add_argument('--output-dir', type=str, default='./models')
     return parser.parse_args()
 
-# Get slice indices from the user
+# Get slicing settings from user
 def get_slice_indices(num_layers):
     n = int(input("How many submodels? ").strip())
     if n < 1:
-        raise ValueError("Submodel count must be >= 1")
+        raise ValueError("The number of submodels must be >= 1")
 
     if n == 1:
         points = [0, num_layers - 1]
@@ -181,7 +175,7 @@ def get_slice_indices(num_layers):
     ]
 
     if n==1:
-        print("Just converting the model.")
+        print("Only converting the model")
     else:
         print(f"Layer index ranges for each submodel: {slice_ranges}")
     
@@ -195,7 +189,7 @@ def main():
     # Load the model from the given path without compilation (for inference/slicing only)
     model = load_model(args.model_path, compile=False)
     
-    # Ask the user for the number of slices and the index of the last layer in each submodel
+    # Ask the user for the number of slices and the index of the last layer in each slice
     num_layers = len(model.layers)
     num_slices, slice_indices = get_slice_indices(num_layers)
 
@@ -203,22 +197,21 @@ def main():
     input_shape = model.layers[0].input_shape[0][1:]
     dummy_input = np.random.rand(1, *input_shape)
 
-    # Perform slicing and model conversion
-    sliced_models = []
+    # Perform slicing and conversion
+    slices = []
     for i in range(num_slices):
-        # Prepare inputs
+        # Prepare inputs for each slice
         if i == 0:
             slice_inputs = {model.layers[0].name: dummy_input}
-            print(model.layers[0].name)
         else:
-            slice_inputs = prepare_slice_inputs(sliced_models[i-1])
+            slice_inputs = prepare_slice_inputs(slices[i-1])
 
         # Slice the model using slice_dnn
-        sliced_model = slice_dnn(model, slice_indices[i], slice_indices[i+1]-1, slice_inputs)
-        sliced_models.append(sliced_model)
+        slice = slice_dnn(model, slice_indices[i], slice_indices[i+1]-1, slice_inputs)
+        slices.append(slice)
 
-        # Convert and save the sliced model to a LiteRT model
-        save_sliced_model(args.output_dir, sliced_model, i)
+        # Convert and save the slice to a LiteRT model
+        convert_save_slice(args.output_dir, slice, i)
 
 if __name__ == "__main__":
     main()
